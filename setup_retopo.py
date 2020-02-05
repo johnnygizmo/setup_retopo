847787d5fe5e4cbb379c6c60a862acbcd385153d
Johnny Matthews <johnny.matthews@gmail.com>
2020-02-04 22:40:36 -0600

    Update setup_retopo.py

--- setup_retopo.py
+++ setup_retopo.py
@@ -16,9 +16,14 @@ class JohnnyGizmoSetupRetopo(bpy.types.Operator):
     bl_label = "Setup Retopo From New Plane"
     bl_options = {'REGISTER', 'UNDO'}
 
-    ypos: bpy.props.FloatProperty(name="Y Position", default=-5.0, min=-100.0, max=100.0)
+    ypos: bpy.props.FloatProperty(name="Y Position", default=-2.0, min=-100.0, max=100.0)
+    scale: bpy.props.FloatProperty(name="Plane Scale", default=0.5, min=.01, max=1.0, precision=2,step=1)
+    
     merge: bpy.props.FloatProperty(name="Automerge Dist", default=0.001, min=0.0, max=0.1,precision=3, step=.1)
     color: bpy.props.FloatVectorProperty(name="Color", description="Object Color", default=(0.0945094, 0.283429, 0.240477,1), options={'ANIMATABLE'}, size=4, subtype='COLOR')
+   
+    cage: bpy.props.BoolProperty(name="Shrink Cage", description="Enable Shrinkwap Cage", default=False)
+   
    
     @classmethod
     def poll(cls, context):
@@ -46,8 +51,9 @@ class JohnnyGizmoSetupRetopo(bpy.types.Operator):
         
         target.select_set(False)
         
+        bpy.ops.transform.resize(value=(self.scale, self.scale, self.scale), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
         bpy.ops.transform.rotate(value=1.5708, orient_axis='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
-        bpy.ops.transform.translate(value=(1, 0, 0), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+        bpy.ops.transform.translate(value=(1*self.scale, 0, 0), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
         bpy.ops.transform.translate(value=(0, self.ypos, 0), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
         bpy.ops.object.origin_set(type='ORIGIN_CURSOR', center='MEDIAN')
         bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
@@ -55,11 +61,13 @@ class JohnnyGizmoSetupRetopo(bpy.types.Operator):
         #modifiers        
         bpy.ops.object.modifier_add(type='SHRINKWRAP')
         bpy.context.object.modifiers["Shrinkwrap"].target = target
+        bpy.context.object.modifiers["Shrinkwrap"].show_on_cage = self.cage
+        
+                
         bpy.ops.object.modifier_add(type='MIRROR')
-        bpy.context.object.modifiers["Mirror"].show_on_cage = True
-
-        #Object Settings
         bpy.context.object.modifiers["Mirror"].use_clip = True
+        
+        #Object Settings
         bpy.context.object.show_in_front = True
         bpy.context.object.color = self.color
         
@@ -78,6 +86,9 @@ class JohnnyGizmoSetupRetopo(bpy.types.Operator):
         bpy.context.space_data.shading.type = 'SOLID'
         bpy.context.space_data.shading.color_type = 'OBJECT'
         bpy.context.space_data.shading.show_backface_culling = True
+        
+        bpy.ops.object.editmode_toggle()
+
         return {'FINISHED'}
 
 
