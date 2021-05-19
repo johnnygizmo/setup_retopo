65c1414c5cb5af989fb7beff706dc78ffc2c0f60
Johnny Matthews <johnny.matthews@gmail.com>
2021-05-19 12:56:55 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import bpy
 bl_info = {
-    "name": "Setup Retopo From New Plane",
+    "name": "Setup Retopology",
     "description": "Do several steps to setup retopology",
     "author": "Johnny Matthews",
     "version": (1, 2),
@@ -13,7 +13,7 @@ bl_info = {
 class JohnnyGizmoSetupRetopo(bpy.types.Operator):
     """Tooltip"""
     bl_idname = "object.johnnygizmo_setup_retopo"
-    bl_label = "Setup Retopo From New Plane"
+    bl_label = "Setup Retopology"
     bl_options = {'REGISTER', 'UNDO'}
 
     ypos: bpy.props.FloatProperty(name="Y Position", default=-2.0, min=-100.0, max=100.0)
