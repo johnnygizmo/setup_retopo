c95473499831c1aa69356bef957d37903633aedb
Johnny Matthews <johnny.matthews@gmail.com>
2022-11-09 20:51:13 -0600

    Update __init__.py
    Small tweaks to plane size

--- __init__.py
+++ __init__.py
@@ -19,8 +19,8 @@ class JohnnyGizmoSetupRetopo(bpy.types.Operator):
     bl_label = "Setup Retopology"
     bl_options = {'REGISTER', 'UNDO'}
 
-    ypos: bpy.props.FloatProperty(name="Y Position", default=-2.0, min=-100.0, max=100.0)
-    scale: bpy.props.FloatProperty(name="Plane Scale", default=0.5, min=.01, max=1.0, precision=2,step=1)
+    ypos: bpy.props.FloatProperty(name="Y Position", default=-1.0, min=-100.0, max=100.0)
+    scale: bpy.props.FloatProperty(name="Plane Size", default=0.1, min=.001, max=1.0, precision=2,step=1)
     
     merge: bpy.props.FloatProperty(name="Automerge Dist", default=0.001, min=0.0, max=0.1,precision=3, step=.1)
     color: bpy.props.FloatVectorProperty(name="Color", description="Object Color", default=(0.0945094, 0.283429, 0.240477,1), options={'ANIMATABLE'}, size=4, subtype='COLOR')
@@ -38,7 +38,7 @@ class JohnnyGizmoSetupRetopo(bpy.types.Operator):
         
         if len(context.selected_objects) ==  1 and context.active_object.type == 'MESH':
             target = context.active_object
-            bpy.ops.mesh.primitive_plane_add(size=2.0, calc_uvs=True, enter_editmode=False, align='WORLD', location=(0.0, 0.0, 0.0), rotation=(0.0, 0.0, 0.0))
+            bpy.ops.mesh.primitive_plane_add(size=1.0, calc_uvs=True, enter_editmode=False, align='WORLD', location=(0.0, 0.0, 0.0), rotation=(0.0, 0.0, 0.0))
             target.select_set(True)
             
         else:
@@ -68,7 +68,7 @@ class JohnnyGizmoSetupRetopo(bpy.types.Operator):
         bpy.ops.transform.rotate(value=1.5708, orient_axis='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
         
         if self.add_mirror == True:
-            bpy.ops.transform.translate(value=(1*self.scale, 0, 0), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+            bpy.ops.transform.translate(value=(0.5*self.scale, 0, 0), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
         
         bpy.ops.transform.translate(value=(0, self.ypos, 0), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
         bpy.ops.object.origin_set(type='ORIGIN_CURSOR', center='MEDIAN')
@@ -109,7 +109,6 @@ class JohnnyGizmoSetupRetopo(bpy.types.Operator):
         bpy.context.space_data.shading.show_backface_culling = True
         
         bpy.ops.object.editmode_toggle()
-        #target.hide_select = True
 
         return {'FINISHED'}
 
